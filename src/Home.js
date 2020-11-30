eb5a2543cd806e7cbed537364fb5f7d01f9fc1a5
Joost Idema <jpp_idema@hotmail.com>
2020-11-30 17:07:41 +0100

    amazon clone

---
+++ src/Home.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import './Home.css';
+import Product from './Product';
+
+function Home() {
+    return (
+        <div className="home">
+            <div className="home__container">
+                <img className="home__image" src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/March/Fuji_TallHero_ShipATW_Modified_en_US_1x._CB419795291_.jpg" alt="" />
+                <div className="home__row">
+                    <Product
+                    id='6235321541' 
+                    title='OnePlus 8T Lunar Silver, 5G Unlocked Android Smartphone U.S. Version, 256GB Storage + 12GB RAM, 120Hz Fluid Display, Quad Camera' 
+                    image='https://images-na.ssl-images-amazon.com/images/I/71GuLTfMJAL._AC_SL1500_.jpg' 
+                    price={699.99} 
+                    rating={5} />
+                    <Product
+                    id='2162423252' 
+                    title='Samsung Galaxy Watch Active 2 (40mm, GPS, Bluetooth) Smart Watch with Advanced Health Monitoring, Fitness Tracking , and Long Lasting Battery - Silver (US Version)'
+                    image='https://images-na.ssl-images-amazon.com/images/I/51bSW9gjoaL._AC_SL1024_.jpg'
+                    price={159.99}
+                    rating={4} />
+                </div>
+                <div className="home__row">
+                    <Product
+                    id='525451135' 
+                    title='Riedel Curly Decanter'
+                    image='https://images-na.ssl-images-amazon.com/images/I/61rMrQzYV-L._AC_SL1500_.jpg'
+                    price={174.69}
+                    rating={5} />
+                    <Product
+                    id='912415151' 
+                    title='Wine Folly: The Essential Guide to Wine'
+                    image='https://images-na.ssl-images-amazon.com/images/I/51S++zq9ydL._SX404_BO1,204,203,200_.jpg'
+                    price={7.95}
+                    rating={5} />
+                    <Product
+                    id='222526678' 
+                    title='Rabbit Automatic Electric Corkscrew Wine Bottle Opener, One Size, Silver'
+                    image='https://images-na.ssl-images-amazon.com/images/I/61o1RRPnjoL._AC_SL1500_.jpg'
+                    price={12.99}
+                    rating={3} />
+                </div>
+                <div className="home__row">
+                    <Product
+                    id='14772341'
+                    title='The Mindful Word Inspirational Mountain Poster - 16 x 42 Inches - Canvas Fabric Poster With Inspirational Quote by Lao Tzu (White / Blue)'
+                    image='https://images-na.ssl-images-amazon.com/images/I/71lk2fndxUL._AC_SL1500_.jpg'
+                    price={49.99}
+                    rating={4} />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
---
+++ src/firebass.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyC1jNwXX-PYAgublM43nZsPh8R33D0Jxhs",
+    authDomain: "clone-63e37.firebaseapp.com",
+    databaseURL: "https://clone-63e37.firebaseio.com",
+    projectId: "clone-63e37",
+    storageBucket: "clone-63e37.appspot.com",
+    messagingSenderId: "731293124809",
+    appId: "1:731293124809:web:a0d23782d0d930c6b5e518",
+    measurementId: "G-RE5X3PR974"
+  };
+  
